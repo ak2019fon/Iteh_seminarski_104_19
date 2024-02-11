8efa379eb2a511e4604482d03385c73756dd99ca
ak2019fon <ak20190104@student.fon.bg.ac.rs>
2024-02-11 13:00:11 +0200

    Stranice i slike

---
+++ Frontend/src/pages/page/AllBooksPage.jsx
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react"
+import Header from "../../components/Header/Header"
+import BookList from "../../components/BookList/BookList"
+import axios from "axios"
+import { useParams } from "react-router-dom"
+
+function AllBooksPage() {
+  const { author } = useParams()
+  const [books, setBooks] = useState([])
+
+  const [fetchingDoneAndDataIsEmpty, setEmpty] = useState(false)
+
+  useEffect(() => {
+    async function fetchData() {
+      setEmpty(false)
+      var response
+      if (author == undefined) {
+        response = await getAllBooks()
+        console.log("ucitava sve knjige")
+      } else {
+        response = await getBooksBySpecifiedAuthor(author)
+      }
+      console.log(response)
+      if (response.status == 200) {
+        setBooks(response.data.data)
+      } else {
+        setEmpty(true)
+      }
+    }
+    fetchData()
+  }, [window.location.pathname])
+
+  if (fetchingDoneAndDataIsEmpty) {
+    return (
+      <div>
+        <Header navbar={true} />
+        <div>
+          <h1>No data found</h1>
+        </div>
+      </div>
+    )
+  }
+
+  if (books.length == 0) {
+    return (
+      <div>
+        <Header navbar={true} />
+        <div class="load"></div>
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <Header navbar={true} />
+      <div className="books">
+        <BookList books={books} />
+      </div>
+    </div>
+  )
+}
+
+async function getAllBooks() {
+  var config = {
+    method: "get",
+    url: "http://127.0.0.1:8000/api/books",
+  }
+
+  let res = axios(config)
+    .then(function (response) {
+      // console.log(JSON.stringify(response.data))
+      return response
+    })
+    .catch(function (error) {
+      // console.log(error)
+      return error
+    })
+
+  return res
+}
+
+async function getBooksBySpecifiedAuthor(author) {
+  var config = {
+    method: "get",
+    url: "http://127.0.0.1:8000/api/books/" + author,
+  }
+
+  let res = axios(config)
+    .then(function (response) {
+      // console.log(JSON.stringify(response.data))
+      return response
+    })
+    .catch(function (error) {
+      console.log(error)
+      return error
+    })
+  return res
+}
+
+export default AllBooksPage
---
+++ Frontend/src/pages/page/UserPage/User.jsx
@@ -0,0 +1,95 @@
+import "./User.css"
+import React, { useState, useEffect } from "react"
+import BookList from "../../../components/BookList/BookList"
+import Button from "../../../components/Button/Button"
+import { Link } from "react-router-dom"
+import Header from "../../../components/Header/Header"
+import Reservations from "../../../components/BookList/Reservations"
+import { Avatar } from "antd"
+import axios from "axios"
+
+function User() {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await getReservations()
+      // console.log(response.reservations)
+      setBooks(response.reservations)
+    }
+    fetchData()
+  }, [])
+
+  function removeBook(reservationId) {
+    let newBooksList = books.filter((element) => element.id != reservationId)
+    setBooks(newBooksList)
+  }
+
+  /*
+  if (books.length == 0) {
+    return (
+      <div>
+        <Header navbar={true} />
+        <div class="load"></div>
+      </div>
+    );
+  }
+*/
+  return (
+    <div>
+      <Header navbar={true} />
+      <br />
+      <br />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Avatar
+          src={`${sessionStorage.getItem("profile_image")}`}
+          style={{ width: "15vh", height: "15vh" }}
+        />
+        <br />
+        <p>{JSON.parse(sessionStorage.getItem("logged_user")).email}</p>
+      </div>
+      <br />
+      <br />
+      <div style={{ border: "solid", width: "100%" }}></div>
+      <br />
+      <p style={{ textAlign: "center" }}>My reservations:</p>
+      <br />
+      <br />
+      <div className="books">
+        <Reservations reservations={books} removeBook={removeBook} />
+      </div>
+    </div>
+  )
+}
+
+async function getReservations() {
+  const user = JSON.parse(sessionStorage.getItem("logged_user"))
+
+  var config = {
+    method: "get",
+    url: `http://127.0.0.1:8000/api/user/${user.id}/reservations`,
+    headers: {
+      Authorization: "Bearer " + sessionStorage.getItem("token"),
+    },
+  }
+
+  let res = axios(config)
+    .then(function (response) {
+      return response.data
+    })
+    .catch(function (error) {
+      // console.log(error)
+      return error
+    })
+
+  return res
+}
+
+export default User
